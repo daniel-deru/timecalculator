d21b97851f598f3a31b398f7b619b6bd4c122833
daniel <danielderu@gmail.com>
2022-01-15 18:54:36 +0200

    time calculator

---
+++ timecalculator.py
@@ -0,0 +1,11 @@
+
+    
+
+    
+
+
+
+# print(add_time("11:59 PM", "24:05", "Wednesday"))
+# print(add_time("11:06 PM", "2:02"))
+# print(add_time("11:40 AM", "0:25"))   
+# print(add_time("8:16 PM", "466:02", "tuesday")) 
